46615a84a7720e3d306fd1f23c0434dbded30cda
aliass532 <56620619+aliass532@users.noreply.github.com>
2026-02-12 20:34:57 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo >> feeds.conf.default
