bea493c5bf0415854929b8bf5b6a8a3d794833fb
aliass532 <56620619+aliass532@users.noreply.github.com>
2026-02-15 21:55:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/1.90.0/1.89.0/g' feeds/packages/lang/rust/Makefile
-sed -i 's/6bfeaddd90ffda2f063492b092bfed925c4b8c701579baf4b1316e021470daac/982f087479302e6d91432efc81852b00/g' feeds/packages/lang/rust/Makefile
+#sed -i 's/1.90.0/1.89.0/g' feeds/packages/lang/rust/Makefile
+#sed -i 's/6bfeaddd90ffda2f063492b092bfed925c4b8c701579baf4b1316e021470daac/982f087479302e6d91432efc81852b00/g' feeds/packages/lang/rust/Makefile
 #sed -i 's/download-ci-llvm=true/download-ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
